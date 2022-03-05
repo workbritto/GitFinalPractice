988d4495476b398e4037cdb3d0147c7e65fa0535
workbritto <workbritto@gmail.com>
2022-03-05 23:41:27 +0530

    initial code

---
+++ src/com/git/GitClass.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class GitClass {
+	private void method1() {
+		System.out.println("1");
+	}
+	private void method2() {
+		System.out.println("2");
+	}
+	private void method3() {
+		System.out.println("3");
+	}
+}
